1afac556eb652150e5675250077c08264ec1d082
nzi19001@byui.edu <nzi19001@byui.edu>
2024-01-16 21:27:18 +0300

    find method

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" },
+    //...
+]
+const results = record.find(superbowlWin);
+function superbowlWin(result) {
+    if (result.result === "W") {
+        return "win";
+    } else {
+        return "undefined"
+    }
+}
+console.log(results)
